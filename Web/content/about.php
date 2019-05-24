7365d0278e53664d810afcd2131f982b46209a9a
Jérémy Huet <21703883@etu.unicaen.fr>
2019-05-24 20:35:48 +0200

    Page à propos

---
+++ Web/content/about.php
@@ -0,0 +1,18 @@
+<?php
+include('../includes/utils_page.php');
+get_header();
+?>
+
+<h1>À propos</h1>
+
+<h2>Justification de nos choix</h2>
+
+
+<p>Pour cette épreuve, nous avons choisi de coder le back-end en PHP pur.
+    Nous avons fait ce choix car tout le monde n'est pas forcément à l'aise avec les mêmes framework (voire avec aucun), et car nous estimions qu'un projet de cette envergure ne nécessitait pas d'utiliser un framework possiblement lourd</p>
+
+<p>Côté front-end, nous avons choisi d'utiliser jQuery pour le Jaavscript, ce qui nous a permis d'utiliser un peu d'AJAX facilement.</p>
+<!-- JUSTIFIER VOTRE CSS-->
+
+<?php
+get_footer();
