7cb61dabbcb9887f042a970136a42d189dc52154
Jérémy Huet <21703883@etu.unicaen.fr>
2019-05-24 14:47:39 +0200

    Base de données

--- Web/database.sql
+++ Web/database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost:3306
--- Généré le :  jeu. 23 mai 2019 à 16:05
+-- Généré le :  ven. 24 mai 2019 à 12:46
 -- Version du serveur :  5.7.24-log
 -- Version de PHP :  7.2.10
 
@@ -21,8 +21,51 @@ SET time_zone = "+00:00";
 --
 -- Base de données :  `24h`
 --
-CREATE DATABASE IF NOT EXISTS `24h` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `24h`;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `commandes`
+--
+
+CREATE TABLE `commandes` (
+  `id` int(11) NOT NULL,
+  `id_type_cafe` int(11) NOT NULL,
+  `id_pays` int(11) NOT NULL,
+  `quantite` int(11) NOT NULL,
+  `id_exportateur` int(11) NOT NULL,
+  `id_importateur` int(11) NOT NULL,
+  `date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `pays`
+--
+
+CREATE TABLE `pays` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `drapeau` text NOT NULL,
+  `capitale` varchar(255) NOT NULL,
+  `nb_habitants` int(11) NOT NULL,
+  `surface` int(11) NOT NULL,
+  `production_arabica` int(11) NOT NULL,
+  `production_robusta` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `type_cafe`
+--
+
+CREATE TABLE `type_cafe` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -34,7 +77,7 @@ CREATE TABLE `users` (
   `id` int(10) NOT NULL,
   `username` varchar(30) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL
+  `role` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -50,9 +93,41 @@ CREATE TABLE `user_meta` (
   `meta_value` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `varietes`
+--
+
+CREATE TABLE `varietes` (
+  `id` int(11) NOT NULL,
+  `id_type_cafe` int(11) NOT NULL,
+  `id_pays` int(11) NOT NULL,
+  `stock` int(11) NOT NULL,
+  `id_exportateur` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 --
 -- Index pour les tables déchargées
 --
+
+--
+-- Index pour la table `commandes`
+--
+ALTER TABLE `commandes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `pays`
+--
+ALTER TABLE `pays`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `type_cafe`
+--
+ALTER TABLE `type_cafe`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Index pour la table `users`
@@ -67,20 +142,50 @@ ALTER TABLE `user_meta`
   ADD PRIMARY KEY (`meta_id`);
 
 --
+-- Index pour la table `varietes`
+--
+ALTER TABLE `varietes`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT pour les tables déchargées
 --
+
+--
+-- AUTO_INCREMENT pour la table `commandes`
+--
+ALTER TABLE `commandes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `pays`
+--
+ALTER TABLE `pays`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `type_cafe`
+--
+ALTER TABLE `type_cafe`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT pour la table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT pour la table `user_meta`
 --
 ALTER TABLE `user_meta`
-  MODIFY `meta_id` int(10) NOT NULL AUTO_INCREMENT;
+  MODIFY `meta_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT pour la table `varietes`
+--
+ALTER TABLE `varietes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
