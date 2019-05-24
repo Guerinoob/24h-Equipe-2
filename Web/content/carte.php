3729a510eb8b3f07fc2dbadff804aa6d5d98720f
DarkZerba <zebra72000@gmail.com>
2019-05-24 17:10:39 +0200

    Ajout de la carte

---
+++ Web/content/carte.php
@@ -0,0 +1,19 @@
+<?php
+include_once('../includes/utils_page.php');
+get_header();
+
+
+get_footer();
+?>
+<div id="chartContainer" style="height: 300px; width: 100%;"></div>
+<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+<div id="map" style="height:800px"></div>
+<link rel="stylesheet" href="https://unpkg.com/leaflet@1.5.1/dist/leaflet.css"
+      integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
+      crossorigin=""/>
+<script src="https://unpkg.com/leaflet@1.5.1/dist/leaflet.js"
+        integrity="sha512-GffPMF3RvMeYyc1LWMHtK8EbPv0iNZ8/oTtHPx9/cc2ILxQ+u905qIwdpULaqDkyBKgOaB57QTMg7ztg8Jm2Og=="
+        crossorigin=""></script>
+<script src="js/html2canvas.js"></script>
+<script src="js/carte.js"></script>
+
---
+++ Web/content/listePays.php
@@ -0,0 +1,7 @@
+<?php
+include_once('../settings.php');
+
+global $db;
+$results = $db->query_get_rows("SELECT nom,production_arabica,production_robusta from pays ");
+echo json_encode($results);
+?>
