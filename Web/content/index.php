7d12447b53cebd73c1977eef654395fc7d0bba39
Quentin <21802953@etu.unicaen.fr>
2019-05-24 14:58:29 +0200

    Login page

--- Web/content/index.php
+++ Web/content/index.php
@@ -12,8 +12,28 @@ if(get_logged_user() != null) {
     <form method="POST" action="">
         <input type="submit" value="Logout" id="logout" name="logout"/>
     </form>
+
+    !DOCTYPE html>
+    <html>
+        <head>
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <title>LMF - Connexion</title>
+            <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+            <link rel="stylesheet" href="bulma.css">
+            <link rel="stylesheet" href="login.css">
+            <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.4/css/bulma.min.css"> -->
+            <script defer src="https://use.fontawesome.com/releases/v5.0.7/js/all.js"></script>
+        </head>
+        <body>
+            <section class="hero has-background-primary">
+
+            </section>
+        </body>
+    </html>
     <?php
 }
+
 
 
 var_dump(get_logged_user());
