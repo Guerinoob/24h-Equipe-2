781c6b84cc4fb9a6a2c884bf1115c7bb7975ebb5
David <dytshi.yt@gmail.com>
2019-05-24 19:27:12 +0200

    image drapeau

--- Web/content/modifierPays.php
+++ Web/content/modifierPays.php
@@ -1,1 +1,43 @@
 <?php
+    include_once('../includes/utils_page.php');
+    get_header();
+
+    global $db;
+
+    $tabPays = $db->query_get_rows("select id,nom from pays");
+
+?>
+
+<select id="pays">
+    <option value="0">-- Selectionner un pays --</option>
+    <?php
+    foreach($tabPays as $pays)
+    {
+        echo "<option value='".$pays['id']."'>".$pays['nom']."</option>";
+    }
+    ?>
+</select>
+
+<div id="la">
+
+</div>
+
+<script language="JavaScript" type="text/javascript">
+    var pays = document.getElementById('pays');
+
+    var chargeDonneesPays = function()
+    {
+        $.post(
+            'recupInfoPays.php',
+            {
+                'id': pays.value
+            },
+            function(response){
+                var la = document.getElementById('la');
+                la.innerHTML = response;
+            }
+        )
+    }
+
+    pays.addEventListener('change', chargeDonneesPays, false);
+</script>
