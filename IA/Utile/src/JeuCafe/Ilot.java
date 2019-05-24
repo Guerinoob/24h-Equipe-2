5d2b4d88e7709be45d5f5443714fc22fba388f80
Sam <sam.go@hotmail.fr>
2019-05-24 23:47:57 +0200

    Puissance 2

--- IA/Utile/src/JeuCafe/Ilot.java
+++ IA/Utile/src/JeuCafe/Ilot.java
@@ -29,4 +29,52 @@ public class Ilot {
         return null;
     }
 
+
+    public ArrayList<Byte> dechiffreEnPuissanceDeDeux(int nb)
+    {
+        //nb doit être >= 1
+
+        ArrayList<Byte> listePuissance = new ArrayList<Byte>();
+
+        if(puissanceProche(nb) == 0) {
+            listePuissance.add(puissanceProche(nb));
+            return listePuissance;
+        }
+        else {
+            while (puissanceProche(nb) != 0) {
+                listePuissance.add(puissanceProche(nb));
+                nb = nb - Math.pow(2, nb);
+            }
+            return listePuissance;
+        }
+    }
+
+    public int puissanceProche(int nb) {
+        int p2 = 1;
+        int i=0;
+
+        while(p2*2 < nb) {
+            p2 = p2*2;
+            i++;
+        }
+
+        if(p2*2 == nb) {
+            i = i+1;
+            return i;
+        }
+        else {
+            return i;
+        }
+    }
+
+
+
+
+
+
+
+    }
+
+
+
 }
